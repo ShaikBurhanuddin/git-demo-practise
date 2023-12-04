53fc222858a14d2a0541fea341789bbafe43598c
shaikburhanuddin <shaiksohail55149@gmail.com>
2023-12-04 23:49:45 +0530

    testing

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Replace 'your-service-name' with the actual name of your LoadBalancer service
+SERVICE_NAME="nodejs-service"
+
+# Get the external IP address of the LoadBalancer service
+EXTERNAL_IP=$(kubectl get services "$SERVICE_NAME" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+if [ -z "$EXTERNAL_IP" ]; then
+  echo "External IP not available yet. Please wait for the LoadBalancer to provision an external IP."
+else
+  echo "External IP Address: $EXTERNAL_IP"
+  
+  # Use curl to make a request to the service using the external IP
+  curl_result=$(curl -sS "http://$EXTERNAL_IP:3001")
+  
+  # Print the result of the curl command
+  echo "Curl Result: $curl_result"
+fi
