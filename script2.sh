33685da1161122a93125c0b4ac4c87ced6b439fe
anw devops <anw-devops@outlook.com>
2023-12-05 19:15:27 +0000

    Updated script2.sh

--- script2.sh
+++ script2.sh
@@ -4,10 +4,6 @@
 AKS_CLUSTER_NAME="burhanuddin-kuber"
 RESOURCE_GROUP="nodejs-rg"
 
-# Set the subscription ID
-SUBSCRIPTION_ID="d038c3e3-1166-4425-a005-78d7ee94df8a"
-az account set --subscription $SUBSCRIPTION_ID
-
 # Check if the AKS cluster is already running
 CLUSTER_STATUS=$(az aks show --name burhanuddin-kuber --resource-group nodejs-rg --query 'powerState.code' -o tsv)
 
