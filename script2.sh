740a9ba43a4738cfbec8349e726dfb210890f574
anw devops <anw-devops@outlook.com>
2023-12-06 17:46:42 +0000

    Updated script2.sh

--- script2.sh
+++ script2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Set your AKS cluster name and resource group
-AKS_CLUSTER_NAME="burhanuddin-kuber"
-RESOURCE_GROUP="nodejs-rg"
+AKS_CLUSTER_NAME="mvn-spring"
+RESOURCE_GROUP="cicd-mvn"
 
 # Check if the AKS cluster is already running
-CLUSTER_STATUS=$(az aks show --name burhanuddin-kuber --resource-group nodejs-rg --query 'powerState.code' -o tsv)
+CLUSTER_STATUS=$(az aks show --name mvn-spring --resource-group cicd-mvn --query 'powerState.code' -o tsv)
 
 if [ "$CLUSTER_STATUS" == "Stopped" ]; then
     # Start the AKS cluster
     echo "Starting AKS cluster..."
-    az aks start --name burhanuddin-kuber --resource-group nodejs-rg
+    az aks start --name mvn-spring --resource-group cicd-mvn
 else
     echo "AKS cluster is already running."
 fi
