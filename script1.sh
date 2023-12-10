a1f81772c3e7ad443e2d169839ebfd76cb16f837
anw devops <anw-devops@outlook.com>
2023-12-10 13:42:17 +0000

    Updated script1.sh

--- script1.sh
+++ script1.sh
@@ -4,69 +4,49 @@
 AKS_CLUSTER_NAME="mvn-spring"
 RESOURCE_GROUP="cicd-mvn"
 
-# Set timeouts and intervals
-START_TIMEOUT=600   # 10 minutes
-STOP_TIMEOUT=300    # 5 minutes
+# Check if the AKS cluster is already running
+CLUSTER_STATUS=$(az aks show --name $AKS_CLUSTER_NAME --resource-group $RESOURCE_GROUP --query 'powerState.code' -o tsv)
 
-function start_cluster() {
+if [ "$CLUSTER_STATUS" == "Stopped" ]; then
+    # Start the AKS cluster
     echo "Starting AKS cluster..."
     az aks start --name $AKS_CLUSTER_NAME --resource-group $RESOURCE_GROUP
 
     # Wait for the cluster to be in a ready state
-    wait_for_cluster_ready $START_TIMEOUT
+    echo "Waiting for the AKS cluster to be ready..."
     az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME
 
-}
+    # Add debugging information
+    set -x
 
-function wait_for_cluster_ready() {
-    local timeout=$1
-    local start_time=$(date +%s)
+    # Set health check threshold
+    HEALTH_CHECK_THRESHOLD=3
+    consecutive_failures=0
+    HEARTBEAT_INTERVAL=10  # Set the interval for heartbeat check in seconds
 
     while true; do
-        CLUSTER_STATUS=$(az aks show --name $AKS_CLUSTER_NAME --resource-group $RESOURCE_GROUP --query 'powerState.code' -o tsv)
+        CLUSTER_HEALTH=$(kubectl get componentstatuses --no-headers | awk '$2=="Healthy"{print $2}')
 
-        if [ "$CLUSTER_STATUS" == "Running" ]; then
+        if [ "$CLUSTER_HEALTH" == "Healthy" ]; then
             echo "AKS cluster is now running and healthy."
             break
+        else
+            ((consecutive_failures++))
+            echo "AKS cluster is not healthy. Consecutive failures: $consecutive_failures"
+
+            if [ $consecutive_failures -ge $HEALTH_CHECK_THRESHOLD ]; then
+                echo "Health check failures exceeded the threshold. Taking corrective action..."
+                # Add your corrective action here, e.g., restarting pods or triggering an alert.
+                break
+            fi
         fi
 
-        if [ $(( $(date +%s) - $start_time )) -ge $timeout ]; then
-            echo "Timed out waiting for the AKS cluster to be ready."
-            exit 1
-        fi
-
-        echo "Waiting for the AKS cluster to be ready..."
-        sleep $HEALTH_CHECK_INTERVAL
+        echo "Waiting for the AKS cluster to be healthy..."
+        sleep $HEARTBEAT_INTERVAL
     done
-}
 
-# Add debugging information
-set -x
-
-# Set health check threshold
-HEALTH_CHECK_THRESHOLD=3
-consecutive_failures=0
-
-while true; do
-    CLUSTER_HEALTH=$(kubectl get componentstatuses --no-headers | awk '$2=="Healthy"{print $2}')
-
-    if [ "$CLUSTER_HEALTH" == "Healthy" ]; then
-        echo "AKS cluster is now running and healthy."
-        break
-    else
-        ((consecutive_failures++))
-        echo "AKS cluster is not healthy. Consecutive failures: $consecutive_failures"
-
-        if [ $consecutive_failures -ge $HEALTH_CHECK_THRESHOLD ]; then
-            echo "Health check failures exceeded the threshold. Taking corrective action..."
-            # Add your corrective action here, e.g., restarting pods or triggering an alert.
-            break
-        fi
-    fi
-
-    echo "Waiting for the AKS cluster to be healthy..."
-    sleep $HEALTH_CHECK_INTERVAL
-done
-
-# Stop debugging information
-set +x
+    # Stop debugging information
+    set +x
+else
+    echo "AKS cluster is already running."
+fi
