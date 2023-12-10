ea56b046dd66c19f58b25b4830530fad019aa140
anw devops <anw-devops@outlook.com>
2023-12-10 10:16:04 +0000

    Updated script2.sh

--- script2.sh
+++ script2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #script1
 # Replace 'your-service-name' with the actual name of your LoadBalancer service
-SERVICE_NAME="nodejs-service"
+SERVICE_NAME="ingress-nginx-controller"
 
 # az account set --subscription 7338b8ab-3b46-420e-becc-da044eebe12c
 az aks get-credentials --resource-group cicd-mvn --name mvn-spring
 
 # Get the external IP address of the LoadBalancer service
-EXTERNAL_IP=$(kubectl get services "$SERVICE_NAME" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+EXTERNAL_IP=$(kubectl get services "$SERVICE_NAME" -n ingress-basic -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 if [ -z "$EXTERNAL_IP" ]; then
   echo "External IP not available yet. Please wait for the LoadBalancer to provision an external IP."
@@ -15,7 +15,7 @@ else
   echo "External IP Address: $EXTERNAL_IP"
   
   # Use curl to make a request to the service using the external IP
-  curl_result=$(curl -sS "http://$EXTERNAL_IP:3001")
+  curl_result=$(curl -sS "http://$EXTERNAL_IP")
   
   # Print the result of the curl command
   echo "Curl Result: $curl_result"
