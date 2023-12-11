491c3160fa5517e0843a6d67ffa3644da19ea77b
anw devops <anw-devops@outlook.com>
2023-12-11 20:15:40 +0000

    Updated image-script.sh

--- image-script.sh
+++ image-script.sh
@@ -2,7 +2,7 @@
 # Get the current image tag from the file
 IMAGE_TAG=$(cat /home/vsts/work/1/a/BuildID/BuildID/buildid.txt)
 # Specify the path to your Kubernetes deployment YAML file
-YAML_FILE="/home/vsts/work/12/s/deployment/deployment.yml"
+YAML_FILE="/home/vsts/work/1/s/deployment/deployment.yml"
 # Replace the image tag in the YAML file
 sed -i "s|image: mvnrepo.azurecr.io/nodejs:.*$|image: mvnrepo.azurecr.io/nodejs:${IMAGE_TAG}|g" $YAML_FILE
 
